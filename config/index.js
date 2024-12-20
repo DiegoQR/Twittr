6c8af0ac374a37113419f66ecaa0985291c7a037
Fátima Quiñones <diegoqr2000@gmail.com>
2024-12-20 10:56:50 -0400

    created config file

--- config/index.js
+++ config/index.js
@@ -1,6 +1,16 @@
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { NODE_ENV, PORT, PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
 
 const config = {
+  dev: NODE_ENV !== 'production',
+  port: PORT || 3000,
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  pgport: PGPORT || 5432,
+};
 
-}
+module.exports = config;
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const config = require("./config")
 const tweetsRouter = require("./routes/tweetsRouter")
 
 const app = express();
-const port = 3000;
+const port = config.port;
 
 app.use(express.json()); //Middleware para interpretar json
 app.use("/tweets", tweetsRouter)
 
 app.listen(port, () => 
-    console.log(`🌎 Server running at http://localhost:${port}`    
-))
+    console.log(`🌎 Server running at http://localhost:${port}`)
+);
