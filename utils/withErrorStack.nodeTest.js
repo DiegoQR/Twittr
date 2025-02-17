bfb06f0fd1a9d7ccb61e8134bff4a594d91a188d
Diego QR <diegoqr2000@gmail.com>
2025-02-17 11:46:53 -0400

    Added a test to try node test runner

---
+++ utils/withErrorStack.nodeTest.js
@@ -0,0 +1,29 @@
+const { describe, it, expect } = require("node:test");
+const assert = require("node:assert");
+const withErrorStack = require("./withErrorStack");
+
+describe("[ utils / withErrorStack ]" , () => {
+    it("should return the error with stack", () => { 
+        // Arrange
+        const error = { message: "Error" };
+        const stack = { TypeError: "Line 32" };
+        const expected = { message: "Error", stack: { TypeError: "Line 32" } }
+        // Act
+        const result = withErrorStack(error, stack, true);
+        // Assert
+        //expect(result).toEqual(expected);
+        assert.deepStrictEqual(result, expected);
+    });
+
+    it("should return the error without stack", () => { 
+         // Arrange
+         const error = { message: "Error" };
+         const stack = { TypeError: "Line 32" };
+         const expected = { message: "Error"}
+         // Act
+         const result = withErrorStack(error, stack, false);
+         // Assert
+         //expect(result).toEqual(expected);
+         assert.deepStrictEqual(result, expected);
+    });
+});
